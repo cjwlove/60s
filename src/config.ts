6f7afd9092cec5c379553ed969c1dcc3a990d065
cjwlove <114965351+cjwlove@users.noreply.github.com>
2026-08-04 08:37:48 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   host: process.env.HOST || '0.0.0.0',
   port: process.env.PORT ? +process.env.PORT : 4399,
-  group: '595941841',
+  group: '260418850',
   author: 'Viki <hi@viki.moe>',
   github: 'https://github.com/vikiboss/60s',
   debug: !!process.env.DEBUG,
@@ -9,4 +9,4 @@ export const config = {
   encodingParamName: process.env.ENCODING_PARAM_NAME || 'encoding',
 }
 
-export const COMMON_MSG = `获取成功。开源地址 ${config.github}，反馈群 ${config.group}。`
+export const COMMON_MSG = `获取成功。QQ ${config.group}。`
